e9a29a0e38aa65f65c3a48ac132ed3137a6321b3
Yuri ZHurakovsky <yuri.zhurakovsky@gmail.com>
2014-09-25 12:54:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,68 @@
-//#include "widget.h"
 #include <QApplication>
 #include <QFileDialog>
 #include <QDebug>
+#include <QDir>
 
+QString process_line(QString &line) {
+    QStringList dataSet = line.split(";");
+    QString azs_id = dataSet.at(0);
+    QString fromPath = dataSet.at(1);
+    QString toPathFile = dataSet.at(2);
+    QStringList onlyPathToList = toPathFile.split('/');
+    QString toPath = "";
+    QString resultString;
+    QString fileName = onlyPathToList.at(onlyPathToList.size() - 1);
+    QString newFileName;
+    QDir currentDir;
+
+    fileName = fileName.trimmed();
+
+    for ( int i = 0; i < onlyPathToList.size() - 1; i++ ) {
+        toPath += onlyPathToList.at(i);
+        toPath += "/";
+    }
+
+    currentDir = QDir::current();
+
+    toPath = "C:/LEARN_QT/renamer" + toPath;
+    if ( currentDir.mkpath(toPath) ) {
+        qDebug() << "new Path created OK" << endl;
+    }
+    newFileName = toPath + fileName;
+
+    if ( QFile::copy(fromPath, newFileName) ) {
+        qDebug() << "File copied OK";
+        resultString = azs_id + ";" + newFileName;
+    } else {
+        qDebug() << "File copied ERROR";
+    }
+
+    return resultString;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //Widget w;
-    //w.show();
-    QString fromDir = QFileDialog::getExistingDirectory(0, "Open dir",
-                                                        "C:/", QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
-    //qDebug() << fromDir << " working directory" << endl;
-    QDir dir(fromDir);
-    QFileInfoList list = dir.entryInfoList();
-    QFile file("out.txt");
-    if ( !file.open(QIODevice::WriteOnly | QIODevice::Text) )
-        return 0;
-
-    QTextStream out(&file);
-
-    for (int i = 0; i < list.size(); ++i) {
-            QFileInfo fileInfo = list.at(i);
-            out << qPrintable(QString("%1").arg(fileInfo.absoluteFilePath())) << endl;
-        }
-
-    QString cp = QDir::currentPath();
-    qDebug() << cp << " my current" << endl;
-    QString newPath  = cp + "/1/2/3";
-    qDebug() << newPath << " my new" << endl;
-    QDir rootPath = QDir::rootPath();
-    if ( rootPath.mkpath(newPath) ) {
-        qDebug() << "new Path created OK" << endl;
+    QString photoList = QFileDialog::getOpenFileName(0, "Open file", "C:/LEARN_QT/", "Text files (*.txt | *.csv)");
+    QFile inFile(photoList);
+    QFile fileOut("out.csv");
+    if (!fileOut.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        qDebug() << "Unable to open output file";
     } else {
-        qDebug() << "new Path wrong" << endl;
+        if (!inFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            qDebug() << "Error opening file";
+        } else {
+            while (!inFile.atEnd()) {
+                QString line = inFile.readLine();
+                QString result = process_line(line);
+                QTextStream out(&fileOut);
+                out << result << endl;
+            }
+            inFile.close();
+        }
     }
-    file.close();
+    fileOut.close();
+    qDebug() << "FINISH!";
+    
     return a.exec();
 }
